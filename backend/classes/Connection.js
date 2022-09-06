971d659f127831bfaae06bac35f54b1f3acf9260
Davide <ceredavide@live.it>
2022-09-06 18:02:58 +0200

    rimosso parte inerente all'asta, ora viene gestita da classe apposita

--- backend/classes/Connection.js
+++ backend/classes/Connection.js
@@ -1,24 +1,8 @@
 const Authentication = require("./Authentication")
-
-const listone = require("../data/Quotazioni_Fantacalcio_Stagione_2022_23.json")
+const Asta = require("./Asta");
 
 const auth = new Authentication();
-const settings = new Map();
-
-const categoriaGiocatoriList = [
-  "Portieri",
-  "Difensori",
-  "Centrocampisti",
-  "Attaccanti"
-]
-
-settings.set("listone", listone)
-settings.set("isAstaOn", false);
-settings.set("categoriaGiocatori", categoriaGiocatoriList[0]);
-settings.set("giocatoreSelezionato", null)
-settings.set("proposte", [])
-settings.set("currentUser", 0)
-
+const asta = new Asta()
 
 class Connection {
   constructor(io, socket) {
@@ -33,37 +17,27 @@ class Connection {
       auth.sendActiveUsers(this.io.sockets)
     });
 
-    socket.on('get-asta-status', () => this.getAstaStatus());
+    socket.on('get-asta-status', () => {
+      asta.getAstaStatus(this.io.sockets)
+    });
 
-    socket.on("active-asta", () => this.activeAsta());
-    socket.on("select-giocatore", (giocatore) => this.selectGiocatore(giocatore))
-    socket.on("send-proposta", (value) => this.sendProposta(value))
+    socket.on("active-asta", () => {
+      asta.activeAsta(this.io.sockets)
+    });
+
+    socket.on("select-giocatore", (giocatore) => {
+      asta.selectGiocatore(this.io.sockets, giocatore)
+    })
+
+    socket.on("send-proposta", (value) => {
+      const user = auth.getUser(this.socket.id)
+      asta.sendProposta(this.io.sockets, user, value)
+    })
+
     socket.on('disconnect', () => this.disconnect());
     //   socket.on('connect_error', (err) => {
     //     console.log(`connect_error due to ${err.message}`);
     //   });
-  }
-
-  getAstaStatus() {
-    const data = Object.fromEntries(settings)
-    this.io.sockets.emit("update-asta-status", { ...data, currentUser: userList[data.currentUser].nome })
-  }
-
-  activeAsta() {
-    settings.set("isAstaOn", true),
-      this.getAstaStatus()
-  }
-
-  selectGiocatore(giocatore) {
-    settings.set("giocatoreSelezionato", giocatore)
-    this.getAstaStatus()
-  }
-
-  sendProposta(value) {
-    const prevState = settings.get("proposte")
-    const user = auth.getUser(this.socket.id)
-    settings.set("proposte", [{ user, value, timestamp: Date.now() }, ...prevState])
-    this.getAstaStatus();
   }
 
   disconnect() {
